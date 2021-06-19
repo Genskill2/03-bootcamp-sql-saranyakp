437627a957b250e6579273512bda2bcdd8d808d2
saranyakp <84776715+saranyakp@users.noreply.github.com>
2021-06-19 19:54:53 +0530

    Update query4.sql

--- query4.sql
+++ query4.sql
@@ -1,4 +1,1 @@
-SELECT subjects.name FROM subjects,books_subjects,books 
-        WHERE books_subjects.subject=subjects.id 
-        AND books_subjects.book=books.id 
-        AND books.title='Atomic Habits';
+SELECT subjects.name FROM subjects,books_subjects,books  WHERE books_subjects.subject=subjects.id AND books.title='Atomic Habits' AND books_subjects.book=books.id  ;
