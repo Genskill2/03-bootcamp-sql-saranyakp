3977688e8e29fdbc1008439b7f23a1418e3e823d
saranyakp <84776715+saranyakp@users.noreply.github.com>
2021-06-19 19:54:20 +0530

    Update query3.sql

--- query3.sql
+++ query3.sql
@@ -1,4 +1,1 @@
-SELECT title FROM books,subjects,books_subjects 
-        WHERE books_subjects.book=books.id 
-        AND books_subjects.subject=subjects.id 
-        AND subjects.name IN('Technology','Politics');
+SELECT title FROM books,subjects,books_subjects WHERE books_subjects.book=books.id AND subjects.name IN('Technology','Politics')  AND books_subjects.subject=subjects.id   ;
